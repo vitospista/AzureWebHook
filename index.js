ad08ab7bdc276329dad64dbbe8219f2afb1467e8
Istvan Vitos <vitospista@gmail.com>
2020-03-20 17:31:39 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const crypto = require('crypto');
+
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    var hmac = crypto.createHmac("sha1", "dTN8xFrUlcqW8Uok8e792S7RfVkTBN5fmuH9ECLsrFTP7hKiccaBSQ==");
+    var signature = hmac.update(JSON.stringify(req.body)).digest("hex");
+    var shaSignature = `sha1=${signature}`;
+    var gitHubSignature = req.headers['x-hub-signature'];
+    if (shaSignature !== gitHubSignature) {
+        context.res = {
+            status: 401,
+            body: "Signatures don't match"
+        };
+        return;
+    }
+
+    if (req.body.pages[0].title){
+        context.res = {
+            body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: ("Invalid payload for Wiki event")
+        }
+    }
+};
